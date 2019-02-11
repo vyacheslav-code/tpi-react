83fe46503a09edda640a4fe0989d381801e37204
m00nc87 <softboymoon@gmail.com>
2019-02-11 17:42:22 +0200

    start page

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 export default () => (
-    <div>
-        <h4>Start Page</h4>
-    </div>
+
+      <main>
+            <div className="row">
+                <div className="col-md-6">
+
+                </div>
+                <div className="col-md-6">
+                    <div className="content">
+                        <h1 id="title">The recorded voice scratched in the speaker.</h1>
+                    </div>
+                </div>
+            </div>
+      </main>
 );
